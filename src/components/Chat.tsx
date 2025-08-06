4e35daf37663df3e39706e42e9079c5b8305b2f4
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-08-06 13:27:52 +0530

    fixed warnings from react

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,28 +7,25 @@ const Chat = () => {
       className="scroll-smooth py-2 flex flex-col gap-4 w-[90%] overflow-y-auto scrollbar-hide items-center"
       style={{ scrollbarWidth: "none" }}
     >
-     
-        {responses.slice().map((response, index) => (
-         <div
-         key={index}
-          className="md:py-8 py-4 flex flex-col justify-end md:w-[90%] lg:w-[88%] w-[100%] gap-3 md:text-[17px] text-[14px] lg:text-[19px] font-medium  my-1"
-        ref={scrollref}
-         >
-            {index % 2 != 0 ? (
+      <div
+       className="md:py-8 py-4 flex flex-col justify-end md:w-[90%] lg:w-[88%] w-[100%] gap-3 md:text-[17px] text-[14px] lg:text-[19px] font-medium  my-1"
+     ref={scrollref}
+      >
+        {responses.map((response, index) => (
+            index % 2 != 0 ? 
          
                 <div key={index} className="self-start  rounded-2xl p-3 px-2 ">
                   {delay && index === responses.length - 1 ? " ... " : response}
                 </div>
-            ) : (
+             : 
               <div
               key={index}
               className="self-end  rounded-2xl p-3 px-2 bg-gray-200 "
               >
                 {" " + response}
               </div>
-            )}
-            </div>
-        ))}
+          ))}
+      </div>
     </div>
   );
 };
