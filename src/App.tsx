896ed69741df2a1ea6ea94bf3f623973730df2eb
M Khadar Basha Jilan <khadarbashajilan001@gmail.com>
2025-09-21 10:34:49 +0530

    ran out of embeddings usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import Chat from "./components/Chat";
-import Inputform from "./components/Inputform";
-import Title from "./components/Title";
-import Prompts from "./components/Prompts";
+// import Chat from "./components/Chat";
+// import Inputform from "./components/Inputform";
+// import Title from "./components/Title";
+// import Prompts from "./components/Prompts";
 import Errorfetching from "./components/Errorfetching";
-import { useBotContext } from "./context/BotContext";
+// import { useBotContext } from "./context/BotContext";
 const App = () => {
-  const { submited,Error } = useBotContext();
+  // const { submited,Error } = useBotContext();
   return (
     <>
       <main className="justify-around p-2 h-screen">
-        {!Error?(
-        <div className="h-[88vh] justify-end md:h-[95vh] bg-gray-100 gap-3 border-3 w-full flex flex-col items-center rounded-2xl p-4">
-          {!submited ? (
-            <>
-              <Title />
-              <Prompts />
-            </>
-          ) : (
-            <Chat />
-          )}
-          <Inputform />
-        </div>
-      ):(
+{/*         {!Error?( */}
+{/*         <div className="h-[88vh] justify-end md:h-[95vh] bg-gray-100 gap-3 border-3 w-full flex flex-col items-center rounded-2xl p-4"> */}
+          // {!submited ? (
+            // <>
+              // <Title />
+              // <Prompts />
+            // </>
+         // ) : (
+           // <Chat />
+          //)}
+          //<Inputform />
+        //</div>
+      //):(
         <Errorfetching></Errorfetching>
-      )}
+      //)}
       </main>
     </>
   );
