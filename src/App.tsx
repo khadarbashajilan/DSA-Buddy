118c8543b2ef3d4961e3cf5a59507456360cf929
M Khadar Basha Jilan <khadarbashajilan001@gmail.com>
2025-09-21 10:37:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,31 @@
 // import Prompts from "./components/Prompts";
 import Errorfetching from "./components/Errorfetching";
 // import { useBotContext } from "./context/BotContext";
+
 const App = () => {
-  // const { submited,Error } = useBotContext();
+  // const { submited, Error } = useBotContext();
+
   return (
     <>
       <main className="justify-around p-2 h-screen">
-{/*         {!Error?( */}
-{/*         <div className="h-[88vh] justify-end md:h-[95vh] bg-gray-100 gap-3 border-3 w-full flex flex-col items-center rounded-2xl p-4"> */}
-          // {!submited ? (
-            // <>
-              // <Title />
-              // <Prompts />
-            // </>
-         // ) : (
-           // <Chat />
-          //)}
-          //<Inputform />
-        //</div>
-      //):(
-        <Errorfetching></Errorfetching>
-      //)}
+        {/* {!Error ? (
+          <div className="h-[88vh] justify-end md:h-[95vh] bg-gray-100 gap-3 border-3 w-full flex flex-col items-center rounded-2xl p-4">
+            {!submited ? (
+              <>
+                <Title />
+                <Prompts />
+              </>
+            ) : (
+              <Chat />
+            )}
+            <Inputform />
+          </div>
+        ) : ( */}
+          <Errorfetching />
+        {/* )} */}
       </main>
     </>
   );
 };
+
 export default App;
